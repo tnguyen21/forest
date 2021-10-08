a521dde576b409567dd258e287c29f0b4f0dee9c
Tommy Bui Nguyen <tommynguyen0512@gmail.com>
2021-10-07 21:01:30 -0400

    add node_level to trie_node

--- trie/trienode.py
+++ trie/trienode.py
@@ -2,6 +2,7 @@ class TrieNode:
     def __init__(
         self,
         char: str,
+        node_level: int = 0,
         is_end_of_word: bool = False,
         children: dict = {},
         parent: 'TrieNode' = None
@@ -11,11 +12,13 @@ class TrieNode:
 
         Args:
             char: character that this trie node represents
+            node_level: level of this node in the trie
             is_end_of_word: bool whether this node is the end of a word
             children: dictionary of children nodes
             parent: parent node
         """
         self.char = char
+        self.node_level = node_level
         self.is_end_of_word = False
         self.children = {}
         self.parent = None
