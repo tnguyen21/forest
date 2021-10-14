1762297d9b871885794acf16a94ec8081a2e210c
Tommy Bui Nguyen <tommynguyen0512@gmail.com>
2021-10-13 23:46:40 -0400

    add test cases

--- tests/test_trie.py
+++ tests/test_trie.py
@@ -1,13 +1,34 @@
 import unittest
-from context import trie
+from .context import trie
 
 
 class TestTrieMethods(unittest.TestCase):
-    def test_insert(self):
+    def test_single_char_insert(self):
         t = trie.Trie()
-        t.insert("bag")
-        result = t.query("b")
-        self.assertEqual(result, [("bag", 1)])
+        t.insert("a")
+        self.assertEqual(t.root.children["a"].char, "a")
+
+    def test_multi_char_insert(self):
+        t = trie.Trie()
+        t.insert("aa")
+        self.assertEqual(t.root.children["a"].char, "a")
+        self.assertEqual(t.root.children["a"].children["a"].char, "a")
+
+    def test_multi_word_same_prefix_insert(self):
+        t = trie.Trie()
+        t.insert("aa")
+        t.insert("ab")
+        self.assertEqual(t.root.children["a"].char, "a")
+        # we expect two children, one to have value 'a', another of value 'b'
+        self.assertEqual(t.root.children["a"].children["a"].char, "a")
+        self.assertEqual(t.root.children["a"].children["b"].char, "b")
+
+    def test_multi_word_diff_prefix_insert(self):
+        t = trie.Trie()
+        t.insert("a")
+        t.insert("b")
+        self.assertEqual(t.root.children["a"].char, "a")
+        self.assertEqual(t.root.children["b"].char, "b")
 
 
 if __name__ == "__main__":
