fc20bfb1a0fac39a8f405bb2d50ee338f67544dc
Tommy Bui Nguyen <tommynguyen0512@gmail.com>
2021-10-28 22:47:57 -0400

    modify driver script to test out init pointers

--- run.py
+++ run.py
@@ -6,13 +6,21 @@ tiny experiments to get minimum functionality working.
 """
 
 from trie import Trie
+import Levenshtein as lv
 
 if __name__ == "__main__":
     t = Trie()
-    t.insert("bag")
-    t.insert("brave")
+    t.add_entry("bag")
+    t.add_entry("brave")
+    t.add_entry("baker")
+    t.add_entry("bake")
+    t.add_entry("egg")
     # print(t.entry_list)
     # print(t.root.char)
     # print(t.root.children)
-    # print(t.root.children['b'].children)
-    print(t.query("b"))
+    # print(t.root.children["b"].children)
+    # print(t.query("b"))
+    # t.similarity_search("b", 2)
+    t.search("")
+    t.dump()
+
