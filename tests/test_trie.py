5040792692bd27c64ecc674dadd559ade4f999bf
Tommy Bui Nguyen <tommynguyen0512@gmail.com>
2021-11-22 21:17:37 -0500

    add comments for tests that need to be written
    ...eventually

--- tests/test_trie.py
+++ tests/test_trie.py
@@ -1,6 +1,7 @@
 import unittest
 from .context import trie
 
+# TODO add fixture for sample trie that we can search easily
 
 class TestTrieMethods(unittest.TestCase):
     def test_single_char_add_entry(self):
@@ -30,6 +31,10 @@ class TestTrieMethods(unittest.TestCase):
         self.assertEqual(t.root.children["a"].char, "a")
         self.assertEqual(t.root.children["b"].char, "b")
 
+class TestTrieSearch(unittest.TestCase):
+    # TODO add test casses for search
+    def example_test(self):
+        return True
 
 if __name__ == "__main__":
     unittest.main()
