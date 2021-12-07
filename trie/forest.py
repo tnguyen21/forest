a9e5b5754a30003f105da702be571938a8d54c94
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2021-12-06 18:03:43 -0600

    add stub for adding and querying entries to forest

--- trie/forest.py
+++ trie/forest.py
@@ -5,7 +5,7 @@
 # ! include phonetics outside of the trie
 TODO
 """
-from typing import List, Tuple
+from typing import Callable, List, Tuple
 from .trie import Trie
 
 # ? How do we store multiple dictionaries and keep phonetic representations of them?
@@ -16,37 +16,70 @@ from .trie import Trie
 # * of tries...but that seems too memory intensive.
 class Forest:
     def __init__(self):
-        """
-        TODO
-        """
         self.tries = []
 
-    def add_trie(self, trie: Trie):
+    def _dummy_phonetics(self, word: str) -> str:
         """
-        TODO
+        Dummy phonetic representation
         """
-        self.tries.append(trie)
+        return word
 
-    def search(self, word: str) -> List[Tuple[str, float, float]]:
+    def add_trie(
+        self,
+        trie: Trie,
+        min_word_len: int,
+        max_word_len: int,
+        phonetic_representation: Callable = _dummy_phonetics,
+        max_edit_distance: int = 2,
+        min_jaro_winkler_sim: float = 0.0,
+    ):
+        """
+        Add Trie to Forest with additional parameters
+        Args:
+        """
+        trie = Trie(max_edit_distance, min_jaro_winkler_sim)
+        trie_data = {
+            "trie": trie,
+            "min_word_len": min_word_len,
+            "max_word_len": max_word_len,
+            "phonetic_representation": phonetic_representation,
+        }
+        self.tries.append(trie_data)
+
+    def add_entry(self, entry: str, phonetic_representation: str):
+        """
+        Add entry to all tries in the forest
+        """
+        for t in self.tries:
+            if len(entry) >= t["min_word_len"] and len(entry) <= t["max_word_len"]:
+                if t["phonetic_representation"] is not None:
+                    # TODO keep track of phonetic representation and original word
+                    phoneticized_entry = t["phonetic_representation"](entry)
+                    t["trie"].add_entry(phoneticized_entry)
+                else:
+                    t["trie"].add_entry(entry, phonetic_representation)
+
+    def search(self, word: str) -> List[Tuple[str, int, float]]:
         """
         Conduct search with query word in parallel on all
         tries in the trie
         Args:
             word: query word
         Return:
-            list of tuples that has potential patch with
-                edit distance and jaro-winkler value
+            list of tuples that has potential matches of query with
+                edit distance and jaro-winkler similiarity
         """
         tentative_results = []
 
-        # TODO get phonetic representation of input word
-        # phonetic_word = phonetic_algorithm(word)
+        # search each trie for the word
+        for i, t in enumerate(self.tries):
+            print(f"Searching Trie {i}/{len(self.tries)}")
 
-        # search each trie for the
-        for i, trie in enumerate(self.tries):
-            print(f"Searching Trie {i}")
-            results = trie.search(word)
-            print("Search results:", results)
+            # convert word into phonetic representation (if any is needed)
+            phoneticized_word = t["phonetic_representation"](word)
+
+            results = t["trie"].search(phoneticized_word)
+            tentative_results.extend(results)
 
         # ? Do we want to just return all results?
         # ? Or do we want some notion of "best" result?
