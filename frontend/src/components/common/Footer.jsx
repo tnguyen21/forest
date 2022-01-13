c29b60f763766ced1cb2d915d50ecf1ba4f02ff7
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2022-01-12 19:23:03 -0600

    basic header, nav, and footer

--- frontend/src/components/common/Footer.jsx
+++ frontend/src/components/common/Footer.jsx
@@ -1,10 +1,52 @@
 import React from "react";
+import styled from "styled-components";
+
+import GitHubIcon from "@mui/icons-material/GitHub";
+
+const FooterContainer = styled.footer`
+  padding: 0 8rem;
+`;
+
+const Credits = styled.div`
+  display: inline-block;
+  margin: auto 0.5rem;
+  font-size: 0.8rem;
+  font-family: "Roboto mono", monospace;
+`;
+
+const CreditLink = styled.a`
+  text-decoration: none;
+  color: rgb(50, 87, 209);
+  font-weight: bold;
+
+  &:visited {
+    color: rgb(50, 87, 209);
+  }
+`;
+
+const SocialIcon = styled.a`
+  float: right;
+  color: black;
+
+  &:visited {
+    color: black;
+  }
+`;
 
 function Footer() {
   return (
-    <footer>
-      <div>Placeholder Footer</div>
-    </footer>
+    <FooterContainer>
+      <Credits>
+        Developed by{" "}
+        <CreditLink href="http://github.com/tnguyen21">
+          Tommy Bui Nguyen
+        </CreditLink>{" "}
+        for Senior Design at Drexel University
+      </Credits>
+      <SocialIcon href="https://github.com/tnguyen21/trie">
+        <GitHubIcon />
+      </SocialIcon>
+    </FooterContainer>
   );
 }
 
--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -1,10 +1,40 @@
 import React from "react";
+import styled from "styled-components";
+
+import { Link } from "react-router-dom";
+
+const HeaderContainer = styled.header`
+  padding: 0 8rem;
+`;
+
+const Title = styled.h1`
+  font-family: "Roboto", sans-serif;
+  font-size: 1.5rem;
+  letter-spacing: 0.15rem;
+  margin: 0;
+  padding: 0;
+  display: inline-block;
+`;
+
+const StyledLink = styled(Link)`
+  font-family: "Roboto mono", monospace;
+  font-size: 1rem;
+  text-decoration: none;
+  padding: 0.5rem;
+  margin: auto 0.5rem;
+  float: right;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
 function Header() {
   return (
-    <div>
-      <div>Header Placeholder</div>
-    </div>
+    <HeaderContainer>
+      <Title>Trie-based NER Demo</Title>
+      <StyledLink to="/">Home</StyledLink>
+      <StyledLink to="/about">About</StyledLink>
+    </HeaderContainer>
   );
 }
 
