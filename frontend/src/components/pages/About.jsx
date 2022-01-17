e63b873342566c9bf4e46b2d046d75223685e80d
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2022-01-16 22:13:10 -0600

    update placeholder content for about page

--- frontend/src/components/pages/About.jsx
+++ frontend/src/components/pages/About.jsx
@@ -1,21 +1,26 @@
 import React from "react";
 import styled from "styled-components";
 import Container from "@mui/material/Container";
+import { Typography } from "@mui/material";
 
 const PageTitle = styled.h2`
   font-family: "Roboto", sans-serif;
   font-size: 1.5rem;
 `;
 
-const Content = styled.div`
-  padding: 500px 0;
-`;
-
 function About() {
   return (
     <Container>
       <PageTitle>About</PageTitle>
-      <Content>content</Content>
+      <Typography
+        sx={{
+          fontFamily: "Roboto",
+          fontSize: "1.5rem",
+          m: 40,
+        }}
+      >
+        Placeholder Content
+      </Typography>
     </Container>
   );
 }
