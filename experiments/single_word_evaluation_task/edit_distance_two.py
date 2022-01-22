167508140c99b8b31a09799885ac00eb2438708d
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2022-01-22 14:15:57 -0600

    add boilerplate to pretasks
    boilerplate code which reads  in trie and training
    data and does respective seraches.
    
    just to get a sense for how long
    running these tasks may take
    
    exact match -> few seconds
    ED 1 -> ~1 min
    ED 2 -> ~30 min?

--- experiments/single_word_evaluation_task/edit_distance_two.py
+++ experiments/single_word_evaluation_task/edit_distance_two.py
@@ -12,16 +12,49 @@ sys.path.insert(
 
 
 from trie import Trie
-import pickle
-import datetime
+from common import load_trie_from_pkl
+from datetime import datetime
+import pandas as pd
 import argparse
 import json
 
-logger_object = {}
+logger_object = {"avg_search_time_per_word": 0, "false_postive_count": 0}
 
 
-def main():
-    return 0
+def main(trie_pkl_path: str, train_data_path: str):
+    """
+    TODO
+
+    Args:
+        trie_pkl_path: TODO
+        train_data_path: TODO
+    """
+    # Load trie
+    trie = load_trie_from_pkl(trie_pkl_path)
+
+    # Set max edit distance of trie to 0 for exact matches
+    trie.max_edit_distance = 2
+
+    # Load data
+    data_df = pd.read_csv(train_data_path)
+
+    # Keep track of amount of time it takes to search for each word
+    exact_match_search_times = []
+
+    for _, query, expected_ed in data_df.itertuples(index=False):
+        start_time = datetime.now()
+        results = trie.search(query)
+        end_time = datetime.now()
+        exact_match_search_times.append((end_time - start_time).total_seconds())
+        print((end_time - start_time).total_seconds())
+
+    # Calculate average search time
+    logger_object["avg_search_time_per_word"] = sum(exact_match_search_times) / len(
+        exact_match_search_times
+    )
+
+    # Print out results
+    print(json.dumps(logger_object))
 
 
 if __name__ == "__main__":
