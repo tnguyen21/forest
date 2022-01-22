25a7ef1796238117017e201bb0fd3b806f6d64d7
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2022-01-21 20:02:42 -0600

    set up exact match pretask for SWT
    do data i/o and set up loop to run queries and log out needed results.
    
    found a bug! more details and logs reported at:
    https://github.com/tnguyen21/trie/issues/1

--- experiments/single_word_evaluation_task/exact_match.py
+++ experiments/single_word_evaluation_task/exact_match.py
@@ -12,30 +12,89 @@ sys.path.insert(
 
 
 from trie import Trie
-import pickle
-import datetime
+from common import load_trie_from_pkl
+from datetime import datetime
+import pandas as pd
 import argparse
 import json
 
-logger_object = {}
+# Refer to README for explanation of logged off values
+logger_object = {"avg_search_time_per_word": 0, "false_postive_count": 0}
 
 
-def main():
-    return 0
+def main(trie_pkl_path: str, train_data_path: str):
+    """
+    TODO
+
+    Args:
+        trie_pkl_path: TODO
+        train_data_path: TODO
+    """
+    # Load trie
+    trie = load_trie_from_pkl(trie_pkl_path)
+
+    # Set max edit distance of trie to 0 for exact matches
+    trie.max_edit_distance = 0
+
+    # Load data
+    data_df = pd.read_csv(train_data_path)
+
+    # Keep track of amount of time it takes to search for each word
+    exact_match_search_times = []
+
+    for _, query, expected_ed in data_df.itertuples(index=False):
+        start_time = datetime.now()
+        results = trie.search(query)
+        end_time = datetime.now()
+        exact_match_search_times.append((end_time - start_time).total_seconds())
+
+        # Check if any results were found for a query given
+        # that expected ED is > 0
+        if expected_ed > 0 and len(results) > 0:
+            logger_object["false_postive_count"] += 1
+            print(
+                f"Query: {query} | "
+                f"Expected ED: {expected_ed} | "
+                f"Found: {results}"
+            )
+
+    # Calculate average search time
+    logger_object["avg_search_time_per_word"] = sum(exact_match_search_times) / len(
+        exact_match_search_times
+    )
+
+    # Print out results
+    print(json.dumps(logger_object))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--data_path",
-        help="Path to .txt file containing words",
+        "--trie_pkl_path",
+        help="Path to .pkl file containing trie loaded with all words",
         default=None,
         required=True,
     )
 
+    parser.add_argument(
+        "--training_data_path",
+        help="Path to .csv file containing training data",
+        default=None,
+        required=True,
+    )
+    parser.add_argument(
+        "--save_data",
+        help="Boolean flag to log out .json file containing results",
+        default=False,
+        required=False,
+    )
+
     args = parser.parse_args()
 
-    main()
+    main(args.trie_pkl_path, args.training_data_path)
 
-    with open(f"seriailzed_trie_size_run_{datetime.now().timestamp()}.json", "w") as f:
-        json.dump(logger_object, f)
+    if args.save_data:
+        with open(
+            f"seriailzed_trie_size_run_{datetime.now().timestamp()}.json", "w"
+        ) as f:
+            json.dump(logger_object, f)
