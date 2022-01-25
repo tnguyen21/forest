8a2a2c9bd48a8880652064ad883e1b16fcb5a878
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2022-01-24 21:28:03 -0600

    add methods for creating ptrie

--- experiments/single_word_evaluation_task/common.py
+++ experiments/single_word_evaluation_task/common.py
@@ -9,7 +9,7 @@ sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 )  # noqa: E501
 
-from trie import Trie
+from trie import Trie, PhoneticTrie
 import pickle
 import pandas as pd
 
@@ -27,6 +27,24 @@ def create_trie(data_path):
             word = word.strip()
             trie.add_entry(word)
     return trie
+
+
+def create_phonetic_trie(data_path):
+    """
+    Loads FDA data from the given path into a Trie
+
+    Assumes FDA data file is a csv with each word on a newline
+    """
+    ptrie = PhoneticTrie()
+
+    # Add trie with default values
+    ptrie.add_trie()
+
+    with open(data_path, "r") as f:
+        for word in f:
+            word = word.strip()
+            ptrie.add_entry(word)
+    return ptrie
 
 
 def pickle_trie(trie, pickle_path):
@@ -47,11 +65,13 @@ def load_trie_from_pkl(pickle_path):
 
 
 if __name__ == "__main__":
-    all_words_trie = create_trie("./datasets/single_word_task/dictionary.csv")
+    all_words_ptrie = create_phonetic_trie("./datasets/single_word_task/dictionary.csv")
 
     # * The below should only need to be done once with the dataset,
     # * and then the pickled trie can be used for most pre-tasks
-    # pickle_trie(all_words_trie, "./datasets/single_word_task/all_words_trie.pkl")
+    pickle_trie(all_words_ptrie, "./datasets/single_word_task/all_words_ptrie.pkl")
 
-    test_trie = load_trie_from_pkl("./datasets/single_word_task/all_words_trie.pkl")
-    print(f"Words in trie same?: {all_words_trie.entry_list == test_trie.entry_list}")
+    test_ptrie = load_trie_from_pkl("./datasets/single_word_task/all_words_ptrie.pkl")
+    print(
+        f"Words in trie same?: {all_words_ptrie.tries[0]['trie'].entry_list == test_ptrie.tries[0]['trie'].entry_list}"
+    )
