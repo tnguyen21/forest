966131af08e8f9b3a43ce62d79599c98c58a47bd
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2022-02-10 20:15:44 -0600

    add helper function to genereate ptrie with all phonetic reprs

--- experiments/single_word_evaluation_task/common.py
+++ experiments/single_word_evaluation_task/common.py
@@ -10,7 +10,10 @@ sys.path.insert(
 )  # noqa: E501
 
 from trie import Trie, PhoneticTrie
-import pickle
+from phonetics import metaphone, dmetaphone
+from jellyfish import soundex
+from fuzzy import nysiis
+import dill as pickle
 import pandas as pd
 
 
@@ -20,6 +23,8 @@ def create_trie(data_path):
 
     Assumes FDA data file is a csv with each word on a newline
     """
+    #! create multiple tries with different phonetic representations
+    #! and then pickle them
     trie = Trie()
 
     with open(data_path, "r") as f:
@@ -47,6 +52,37 @@ def create_phonetic_trie(data_path):
     return ptrie
 
 
+def create_phonetic_trie_all_phonetics(data_path):
+    """
+    Loads FDA data from the given path into multiple tries
+    Each trie uses a different phonetic algorithm
+    """
+    ptrie = PhoneticTrie()
+
+    # Add trie with default values
+    ptrie.add_trie()
+
+    # Add trie with dmetaphone
+    # dmetaphone returns two phonetic representations, take the first
+    dmetaphone_wrapper = lambda word: dmetaphone(word)[0]
+    ptrie.add_trie(phonetic_representation=dmetaphone_wrapper)
+
+    # Add trie with metaphone
+    ptrie.add_trie(phonetic_representation=metaphone)
+
+    # Add trie with nysiis
+    ptrie.add_trie(phonetic_representation=nysiis)
+
+    # Add trie with soundex
+    ptrie.add_trie(phonetic_representation=soundex)
+
+    with open(data_path, "r") as f:
+        for word in f:
+            word = word.strip()
+            ptrie.add_entry(word)
+    return ptrie
+
+
 def pickle_trie(trie, pickle_path):
     """
     Pickles the given trie to the given path
@@ -65,13 +101,19 @@ def load_trie_from_pkl(pickle_path):
 
 
 if __name__ == "__main__":
-    all_words_ptrie = create_phonetic_trie("./datasets/single_word_task/dictionary.csv")
+    all_words_ptrie = create_phonetic_trie_all_phonetics(
+        "./datasets/single_word_task/dictionary.csv"
+    )
 
     # * The below should only need to be done once with the dataset,
     # * and then the pickled trie can be used for most pre-tasks
-    pickle_trie(all_words_ptrie, "./datasets/single_word_task/all_words_ptrie.pkl")
+    pickle_trie(
+        all_words_ptrie, "./datasets/single_word_task/all_words_all_phonetics_ptrie.pkl"
+    )
 
-    test_ptrie = load_trie_from_pkl("./datasets/single_word_task/all_words_ptrie.pkl")
+    test_ptrie = load_trie_from_pkl(
+        "./datasets/single_word_task/all_words_all_phonetics_ptrie.pkl"
+    )
     print(
         f"Words in trie same?: {all_words_ptrie.tries[0]['trie'].entry_list == test_ptrie.tries[0]['trie'].entry_list}"
     )
