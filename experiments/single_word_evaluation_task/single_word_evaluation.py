07022f3495a0a6d0c90bf616996a1af88a6eb16c
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2022-02-15 10:53:36 -0600

    swt performance improvement, metric corrections, clean up imports
    Lots of changes I haven't committed + pushed over the last few days.
    
    - speed up generating dataset by avoiding dataframe.iloc every time
    row is appended. use native python list and convert to df
    at the end. MUCH FASTER
    - clean up unused/accidently imports
    - avoid sklearn.classification_report. assumes multi-class problem. use
    individual metrics functions from sklearn
    - also use new ptrie interface that lets us specify edit distance
    to search with

--- experiments/single_word_evaluation_task/single_word_evaluation.py
+++ experiments/single_word_evaluation_task/single_word_evaluation.py
@@ -3,12 +3,8 @@ TODO
 """
 
 # TODO hacky way to add to python path -- need to find better way to mangage imports
-from asyncio.log import logger
-from re import A
 import sys
 import os
-
-from sklearn.metrics import classification_report
 
 sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
@@ -19,8 +15,9 @@ from trie import PhoneticTrie
 from datetime import datetime
 from common import load_trie_from_pkl
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report, roc_curve, auc
-import pickle
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import dill as pickle
+from phonetics import dmetaphone  # needed to load in trie
 import argparse
 import json
 import numpy as np
@@ -31,7 +28,9 @@ from pprint import pprint
 logger_object = {}
 
 
-def generate_data_set(ptrie: PhoneticTrie, data_df: pd.DataFrame) -> pd.DataFrame:
+def generate_data_set(
+    ptrie: PhoneticTrie, data_df: pd.DataFrame, edit_distance: int = 2
+) -> pd.DataFrame:
     """
     Args:
         ptrie: PhoneticTrie to use for searching
@@ -54,19 +53,20 @@ def generate_data_set(ptrie: PhoneticTrie, data_df: pd.DataFrame) -> pd.DataFram
         "label",
     ]
 
-    train_df = pd.DataFrame(columns=train_columns)
+    to_df_list = []
     for idx, word, search, ed in data_df.itertuples():
-        print(f"{idx} Searching for {search}")
+        print(f"{idx} Searching for {search} with edit distance {edit_distance}")
         results = ptrie.search(
             search,
+            max_edit_distance=edit_distance,
             metaphone_output=True,
             dmetaphone_output=True,
             soundex_output=True,
             nysiis_output=True,
         )
-
-        #! this is slow, but it works
         for result in results:
+            #! if result is empty, should still add a row when creating a dataset
+            #! failed searched
             label = 1 if result["result"] == word else 0
 
             append_row = [
@@ -86,21 +86,27 @@ def generate_data_set(ptrie: PhoneticTrie, data_df: pd.DataFrame) -> pd.DataFram
             ]
 
             # append row to df
-            train_df.loc[len(train_df)] = append_row
+            to_df_list.append(append_row)
+
+    train_df = pd.DataFrame(to_df_list, columns=train_columns)
 
     return train_df
 
 
 def main(
-    trie_pkl_path: str, train_data_path: str, validation_data_path: str, data_dir: str
+    trie_pkl_path: str,
+    train_data_path: str,
+    validation_data_path: str,
+    data_dir: str,
+    edit_distance: int,  # this ED is temp
 ):
     """ """
     # Load phonetic_trie
     phonetic_trie = load_trie_from_pkl(trie_pkl_path)
-    #! Manually setting trie edit distance here for experiments, probably don't want
-    #! to do this in the future
-    phonetic_trie.tries[0]["trie"].max_edit_distance = 3
-    logger_object["trie_edit_distance"] = 3
+    logger_object["trie_edit_distance"] = edit_distance
+
+    #! when word is searched and no result if found, this should be a penalty against scoring
+    #! it's false
 
     # Load data
     train_prep_df = pd.read_csv(train_data_path)
@@ -109,7 +115,7 @@ def main(
     if data_dir == None:
         # Generate trian data set
         start_time = datetime.now()
-        train_df = generate_data_set(phonetic_trie, train_prep_df)
+        train_df = generate_data_set(phonetic_trie, train_prep_df, edit_distance)
         end_time = datetime.now()
         logger_object["generate_train_data_set_time"] = (
             end_time - start_time
@@ -117,7 +123,7 @@ def main(
 
         # Generate validation data set
         start_time = datetime.now()
-        val_df = generate_data_set(phonetic_trie, val_prep_df)
+        val_df = generate_data_set(phonetic_trie, val_prep_df, edit_distance)
         end_time = datetime.now()
         logger_object["generate_val_data_set_time"] = (
             end_time - start_time
@@ -125,10 +131,12 @@ def main(
 
         # Save data set
         train_df.to_csv(
-            "./experiments/single_word_evaluation_task/train_df_ed3.csv", index=False
+            f"./experiments/single_word_evaluation_task/train_df_ed{edit_distance}.csv",
+            index=False,
         )
         val_df.to_csv(
-            "./experiments/single_word_evaluation_task/val_df_ed3.csv", index=False
+            f"./experiments/single_word_evaluation_task/val_df_ed{edit_distance}.csv",
+            index=False,
         )
     else:
         # Load data set
@@ -154,8 +162,8 @@ def main(
 
     # Calculate metrics
     y_pred = classifier.predict(X_val)
-    metrics = classification_report(y_val, y_pred, output_dict=True)
-    logger_object["metrics_w_phonetics"] = metrics
+    f1_score_w_phonetics = f1_score(y_val, y_pred)
+    logger_object["f1_score_w_phonetics"] = f1_score_w_phonetics
 
     # -- Compare models and performance for non-phonetic representations --
     train_no_phonetics_df = train_df.drop(
@@ -201,10 +209,8 @@ def main(
     ).total_seconds()
 
     y_no_phonetics_pred = no_phonetics_classifier.predict(X_no_phonetics_val)
-    metrics_no_phonetics = classification_report(
-        y_no_phonetics_val, y_no_phonetics_pred, output_dict=True
-    )
-    logger_object["metrics_no_phonetics"] = metrics_no_phonetics
+    f1_score_no_phonetics = f1_score(y_no_phonetics_val, y_no_phonetics_pred)
+    logger_object["f1_score_no_phonetics"] = f1_score_no_phonetics
 
     # -- Compare models and performance w only dmetaphone --
     train_dmetaphone_df = train_df.drop(
@@ -243,10 +249,8 @@ def main(
     ).total_seconds()
 
     y_dmetaphone_pred = dmetaphone_classifier.predict(X_dmetaphone_val)
-    metrics_dmetaphone = classification_report(
-        y_dmetaphone_val, y_dmetaphone_pred, output_dict=True
-    )
-    logger_object["metrics_dmetaphone"] = metrics_dmetaphone
+    f1_score_dmetaphone = f1_score(y_dmetaphone_val, y_dmetaphone_pred)
+    logger_object["f1_score_dmetaphone"] = f1_score_dmetaphone
 
 
 if __name__ == "__main__":
@@ -290,6 +294,7 @@ if __name__ == "__main__":
         args.training_data_path,
         args.validation_data_path,
         args.data_from_dir,
+        0,
     )
     script_end_time = datetime.now()
 
@@ -301,6 +306,6 @@ if __name__ == "__main__":
 
     if args.save_data:
         with open(
-            f"single_word_evaluation_run_ed3_{datetime.now().timestamp()}.json", "w"
+            f"single_word_evaluation_run_ed{0}_{datetime.now().timestamp()}.json", "w"
         ) as f:
             json.dump(logger_object, f, indent=2)
