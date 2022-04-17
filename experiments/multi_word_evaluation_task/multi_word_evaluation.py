a65bebeb8ba089ae066706597f66fb2f9d6f0f90
Tommy Bui Nguyen <tommynguyen0512@gmail.com>
2022-04-17 17:35:29 +0000

    add testing script for dev

--- experiments/multi_word_evaluation_task/multi_word_evaluation.py
+++ experiments/multi_word_evaluation_task/multi_word_evaluation.py
@@ -2,5 +2,24 @@
 TODO
 """
 
+# TODO hacky way to add to python path -- need to find better way to mangage imports
+import sys
+import os
+
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+)  # noqa: E501
+
+from trie import Forest
+import pandas as pd
+from tqdm import tqdm
+from pprint import pprint
+
 if __name__ == "__main__":
-    print("placeholder")
+    forest = Forest()
+    imbd_df = pd.read_csv("datasets/imdb_movie_titles/-a.csv")
+    for _, id, title in tqdm(imbd_df.itertuples(), desc="Processing IMDB titles..."):
+        forest.add_phrase(id, title)
+    
+    pprint(forest.concept_id_expression_gazetteer)
+    pprint(forest.word_expression_gazetteer)
