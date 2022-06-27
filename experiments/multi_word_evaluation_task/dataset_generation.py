12242e33d64c484b10041a2338cbf0ba70057d8d
Tommy Bui Nguyen <tommynguyen0512@gmail.com>
2022-06-27 07:30:41 -0400

    add additional logic for filler words between expr when generating datasets

--- experiments/multi_word_evaluation_task/dataset_generation.py
+++ experiments/multi_word_evaluation_task/dataset_generation.py
@@ -5,12 +5,38 @@ Code which generates evaluation dataset for multi-word matching task.
 """
 
 from pprint import pprint
+from typing import Set, List
 import random
 import csv
 
-from more_itertools import distinct_combinations
+def add_stop_words(dictionary_words: Set[str], sentence: List[str]) -> str:
+    """
+    add [0, 5] filler words between expressions in sentence
+    where filler words is a random choice from: prepositions/stop words,
+    1000 randomly chosen words from PWN, and a set of words from the input
+    dict
+    
+    args:
+        dictionary_words: set of individual words from terms in dict
+        sentence: constructed sentence with annotations for training
+    """
+    # TODO move this out of this function eventually to prevent multiple reading-ins
+    pwn_stop_words_path = "datasets/1000_gazetteer_entries.txt"
+    with open(pwn_stop_words_path, "r") as f:
+        pwn_stop_words = [word.strip() for word in f.readlines()]
 
-filler_words = ["uh", "um", "er", "ah"]
+    # ? include all prepositions
+    stop_words = ["a", "and", "the", "without", "from", "to", "since", "until"]    
+    
+    # list -> set conversion adds additional randomness since
+    # sets do not have "ordering" of entries -- do we want this?
+    filler_words = stop_words + pwn_stop_words + list(dictionary_words)
+
+    # add [0, 5] filler words between expressions in sentence
+    for _ in range(random.randint(0, 5)):
+        sentence += [random.choice(filler_words)]
+    
+    return sentence
 
 def generate_datasets(
     dictionary_path: str,
@@ -27,6 +53,13 @@ def generate_datasets(
         reader = csv.reader(f)
         dictionary = [tuple(row) for row in list(reader)]
     
+    # create set of unique words from dictionary
+    dictionary_terms_set = set()
+    for (_, entry) in dictionary:
+        # assume words in entry are separated by space
+        for word in entry.split(' '):
+            dictionary_terms_set.add(word)
+
     for (cuid, entry) in dictionary:
         # randomly choose how many sentences will start with entry
         number_of_sentences_that_start_with_entry = random.randint(1, 3)
@@ -34,18 +67,16 @@ def generate_datasets(
         number_of_additional_expressions = random.randint(1, 5)
         
         # TODO save off labels for when expressions start and end
-        # ? token or character position
-        # ? separating cuids
+        # ? token or character position -- token position
+        # ? separating cuids -- don't mix expressions with same CUID
         for _ in range(number_of_sentences_that_start_with_entry):
             # begin sentence with entry
             sentence = [entry]
             
             # add additional expressions to sentence
             for _ in range(number_of_additional_expressions):
-                # number of filler words between expressions in sentence
-                for _ in range(random.randint(0, 5)):
-                    sentence += [random.choice(filler_words)]
-                
+                add_stop_words(dictionary_terms_set, sentence=sentence)
+
                 # add another expression to the sentence, 2nd element in tuple
                 sentence += [random.choice(dictionary)[1]]
         print(" ".join(sentence))
@@ -54,3 +85,4 @@ def generate_datasets(
 if __name__ == "__main__":
     test_file = "datasets\imdb_movie_titles\-of.csv"
     generate_datasets(test_file, "")
+    # add_stop_words({})
