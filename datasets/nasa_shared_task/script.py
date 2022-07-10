1e22cffca7e0e27f32faadfff00f2709f135d167
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2022-07-10 17:44:40 -0400

    write helper script to convert nasa dict into expected format

---
+++ datasets/nasa_shared_task/script.py
@@ -0,0 +1,16 @@
+import csv
+
+if __name__ == "__main__":
+    dict_txt_path = "HEXTRATO_dictionary.txt"
+    csv_output_path = "HEXTRATO_dictionary.csv"
+
+
+    with open(dict_txt_path, "r") as txt_file:
+        with open(csv_output_path, "w") as csv_file:
+            output_writer = csv.writer(csv_file, delimiter=",")
+
+            for line in txt_file:
+                stripped_line = line.strip()
+                csv_row = [stripped_line, stripped_line]
+                output_writer.writerow(csv_row)
+
