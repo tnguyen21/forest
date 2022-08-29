41b1e828e19dc7b2cb05a4c8f514b54530bada59
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2022-08-28 21:16:59 -0400

    clean up script and comment

--- experiments/multi_word_evaluation_task/multi_word_evaluation.py
+++ experiments/multi_word_evaluation_task/multi_word_evaluation.py
@@ -10,25 +10,20 @@ sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 )  # noqa: E501
 
-from trie import Forest
-import pandas as pd
-from tqdm import tqdm
-from pprint import pprint
 import dill as pickle
 
 if __name__ == "__main__":
-    forest = Forest()
-    # dict = pd.read_csv("datasets/umls_small_dictionary/dictionary.csv")
-    dict = pd.read_csv("datasets/nasa_shared_task/HEXTRATO_dictionary.csv")
-    for _, id, term in tqdm(dict.itertuples(), desc="Processing terms..."):
-        forest.add_phrase(id, term)
+    #! forest and LR model should be created and pkl'd out
+    #! after running dataset_generation.py
 
-    forest.create_tries()
-    forest.calculate_determining_scores()
+    # load forest
+    forest_path = "test_forest.pkl"
+    with open(forest_path, "rb") as f:
+        forest = pickle.load(f)
+    forest.logistic_regression_score = 0.5
 
     # load logistic regression model
-    sample_sentence_output_path = "experiments/multi_word_evaluation_task/"
-    with open(sample_sentence_output_path + "lr_model.pkl", "rb") as f:
+    with open("experiments/multi_word_evaluation_task/lr_model.pkl", "rb") as f:
         lr_models = pickle.load(f)
 
     forest.set_logistic_regression_model(lr_models)
